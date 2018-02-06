65da67b317c370a4d7eedd8576c51ec319b04f44
Joshua Palacios <palacios.joshua@gmail.com>
2018-02-06 04:22:55 +0000

    automatically backed up by learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -5,7 +5,7 @@ class Bands extends Component {
   render() {
     return(
       <ul>
-        {this.props.store.bands.map(band => band)}
+        {this.props.store.getState().bands.map(band => <Band band={band} store={this.props.store}/>)}
       </ul>
     );
   }
