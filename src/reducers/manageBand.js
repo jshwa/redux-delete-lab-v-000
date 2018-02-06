48e23bbc870909c193ad976a4d42423eaf975b92
Joshua Palacios <palacios.joshua@gmail.com>
2018-02-06 04:10:48 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -4,6 +4,8 @@ export default function manageBand(state = {
   switch (action.type) {
     case 'ADD_BAND':
       return { bands: state.bands.concat(action.band) }
+    case 'DELETE_BAND':
+      return { bands: state.bands.filter(band => band.id !== action.band.id)}
     default:
       return state;
   }
